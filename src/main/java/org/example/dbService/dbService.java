f6f0b7c45baf7691d9cfb4e1f951a5ddf8e9e2da
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-04-15 17:13:22 +0500

    +DataBase Service

--- src/main/java/org/example/dbService/dbService.java
+++ src/main/java/org/example/dbService/dbService.java
@@ -1,5 +1,16 @@
 package org.example.dbService;
 
+import java.sql.*;
+
 public class dbService {
-    
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "root123";
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+    }
+
+    public  Result
 }

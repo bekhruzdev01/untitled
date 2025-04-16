a14509d613b4e47e861b9f6a6e06a82ddd13d886
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-04-16 23:54:01 +0500

    added edit functions

--- src/main/java/org/example/dbService/DbService.java
+++ src/main/java/org/example/dbService/DbService.java
@@ -36,6 +36,24 @@ public class DbService {
             System.out.println(rs.getString(2));
         }
     }
+    public Result editRegion(Integer id, String name, Integer countryId, Result result) throws SQLException, ClassNotFoundException {
+        String query = "{call region(?,?,?,?,?)}";
+        CallableStatement pS = getConnection().prepareCall(query);
+
+        pS.setInt(1, id);
+        pS.setString(2, name);
+        pS.setInt(3, countryId);
+
+        pS.registerOutParameter(4, Types.VARCHAR);
+        pS.registerOutParameter(5, Types.BOOLEAN);
+
+        pS.execute();
+        result.setMessage(pS.getString(4));
+        result.setSuccess(pS.getBoolean(5));
+
+        return result;
+    }
+
     public String deleteRegion(Integer id) throws SQLException, ClassNotFoundException {
         String query = "delete from region where id = ?";
         PreparedStatement preparedStatement = getConnection().prepareStatement(query);

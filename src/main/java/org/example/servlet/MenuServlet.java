0b4713429cf6c5495f78c9c4f9299ce4e21b7f7c
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-04-17 22:32:31 +0500

    servlet +

--- src/main/java/org/example/servlet/MenuServlet.java
+++ src/main/java/org/example/servlet/MenuServlet.java
@@ -1,5 +1,18 @@
 package org.example.servlet;
 
-public class MenuServlet extends Se {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MenuServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>salom</h1>");
+
+    }
 }

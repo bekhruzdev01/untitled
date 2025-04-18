d0eca105f34be571585db78e18d87cb54e4c6959
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-04-18 21:50:12 +0500

    gson to json

--- src/main/java/org/example/servlet/MenuServlet.java
+++ src/main/java/org/example/servlet/MenuServlet.java
@@ -1,6 +1,7 @@
 package org.example.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,76 +12,12 @@ import org.example.model.Book;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/books")
 public class MenuServlet extends HttpServlet {
-    @SneakyThrows
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        DbService dbService = new DbService();
-        StringBuilder str = new StringBuilder();
-        int tr = 1;
-        for (Book book : dbService.getBooks()) {
-            str.append(
-                    "        <tr>\n" +
-                            "            <td>" + tr + "</td>\n" +
-                            "            <td>" + book.getName() + "</td>\n" +
-                            "            <td>" + book.getPrice() + "so'm</td>\n" +
-                            "            <td>" + book.getWriter() + "</td>\n" +
-                            "            <td>" + book.getYear() + "-yil</td>\n" +
-                            "            <td>\n" +
-                            "                <button class=\"btn btn-warning\" onClick=\"editBook()\">Taxrirlash</button>\n" +
-                            "            </td>\n" +
-                            "            <td>\n" +
-                            "                <button class=\"btn btn-danger\">O'chirish</button>\n" +
-                            "            </td>\n" +
-                            "        </tr>\n"
-            );
-            tr++;
-        }
-        writer.write("<!doctype html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta name=\"viewport\"\n" +
-                "          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n" +
-                "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" +
-                "    <title>Book</title>\n" +
-                "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css\">\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<div class=\"container\">\n" +
-                "    <h1 class=\"text-center text-success\">Kitoblar sahifasi</h1>\n" +
-                "    <button class=\"btn btn-success\">Saqlash</button>\n" +
-                "    <table class=\"table\">\n" +
-                "        <thead>\n" +
-                "        <tr>\n" +
-                "            <td>T/r</td>\n" +
-                "            <td>Nomi</td>\n" +
-                "            <td>Narxi</td>\n" +
-                "            <td>Yozuvchisi</td>\n" +
-                "            <td>Yili</td>\n" +
-                "            <td colspan=\"2\">Sozlamalar</td>\n" +
-                "        </tr>\n" +
-                "        </thead>\n" +
-                "        <tbody>\n" +
-                str +
-                "        </tbody>\n" +
-                "    </table>\n" +
-                "</div>\n" +
-                "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js\"></script>\n" +
-                "<script>\n" +
-                "    function editBook() {\n" +
-                "      fetch(\"http://localhost:8080/edit\")\n" +
-                "        .then(response => response.text())\n" +
-                "        .then(alert(\"Ishladi\"))\n" +
-                "        .catch(error => {\n" +
-                "          document.getElementById(\"result\").innerText = \"Xatolik yuz berdi\";\n" +
-                "          console.error(error);\n" +
-                "        });\n" +
-                "    }\n" +
-                "  </script>" +
-                "</body>\n" +
-                "</html>");
-
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.
     }
 }
---
+++ src/main/resources/html/script.js
@@ -0,0 +1,11 @@
+let str;
+const book =[
+    ()=> {
+        fetch("http://localhost:8080/books")
+            .then(resp => resp.json())
+    }];
+function str(){
+    book.map(()=>{
+        book.
+    })
+}

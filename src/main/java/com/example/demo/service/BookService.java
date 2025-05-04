f34ad848c6c4902ebb14e6843d2adde3719c058f
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-04 23:23:43 +0500

    feat: spring boot

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,5 +1,32 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
-    
+    private final BookRepository repo;
+
+    public BookService(BookRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Book> getAll() {
+        return repo.findAll();
+    }
+
+    public Book getById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public void save(Book book) {
+        repo.save(book);
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
 }

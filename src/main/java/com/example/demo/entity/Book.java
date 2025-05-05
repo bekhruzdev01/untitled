20814ba2a1ae16832f74a00ad3389e1a8b558276
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-05 23:24:53 +0500

    feat: Update Book entity and application properties
    - Modified `Book.java` to include new fields and methods.
    - Updated `application.properties` for configuration changes.

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Book {
     @Id

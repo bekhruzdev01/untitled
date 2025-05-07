4c2d30c55b7a475a5166314f75fecc0cd015a3af
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-07 23:32:03 +0500

    Feat: Search by country name

--- src/main/java/com/example/demo/repository/CountryRepository.java
+++ src/main/java/com/example/demo/repository/CountryRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.entity.Book;
 import com.example.demo.entity.Country;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
-   
+   List<Book> findByCouList(String title, String author);
     
 }

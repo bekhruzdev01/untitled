3dd4578e28914afdc03a71515f5592d1d24ab86e
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-07 23:05:58 +0500

    Feature: Add country entity and controller

--- src/main/java/com/example/demo/entity/Country.java
+++ src/main/java/com/example/demo/entity/Country.java
@@ -1,5 +1,20 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Country {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
 }

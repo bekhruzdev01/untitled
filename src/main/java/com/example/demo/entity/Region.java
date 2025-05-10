42d0450dbf421597140d723635d1138eae5ae982
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-10 23:11:33 +0500

    Add Region entity to the project

---
+++ src/main/java/com/example/demo/entity/Region.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+}

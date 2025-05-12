eef9e5f872eec0f553b22fea4146ea44618792b4
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-12 22:46:28 +0500

    feat: initialize my-app project and update Country entity
    - Added initial setup for the `my-app` project, including:
        - `.gitignore`, `README.md`, and package files.
        - Public assets such as `index.html`, `manifest.json`, and `robots.txt`.
        - Core source files including `App.js`, `index.js`, and component/layout structure.
        - Pages like `AuthForm`, `Cbu`, `First`, and `Last`.
        - Firebase configuration and test setup.
    
    - Updated `Country.java` in the `entity` package.

--- src/main/java/com/example/demo/entity/Country.java
+++ src/main/java/com/example/demo/entity/Country.java
@@ -1,7 +1,10 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,6 +12,9 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

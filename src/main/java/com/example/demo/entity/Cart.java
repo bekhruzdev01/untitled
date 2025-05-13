168ec02a3e723dab0de59d4ac0bfa032f560558b
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-13 23:51:12 +0500

    Refactor: Replace Card entity with Cart entity
    - Removed `Card.java` entity as it is no longer needed.
    - Added `Cart.java` entity to support new functionality.

---
+++ src/main/java/com/example/demo/entity/Cart.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}

05b8f947280d17cc1d17b71c38a13b334d247615
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-13 23:42:06 +0500

    feat: update User entity
    - Made improvements to the User entity in the codebase.
    - This change enhances functionality and ensures better maintainability.

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,21 @@
 package com.example.demo.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // ADMIN yoki CUSTOMER
 }

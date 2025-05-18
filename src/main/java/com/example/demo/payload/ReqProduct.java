29bada7883bcf9e82bde66a8b5a28daba8f515f2
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-18 23:51:38 +0500

    Refactor ReqProduct.java: improve code readability and structure

--- src/main/java/com/example/demo/payload/ReqProduct.java
+++ src/main/java/com/example/demo/payload/ReqProduct.java
@@ -3,11 +3,16 @@ package com.example.demo.payload;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class ReqProduct {
+    @NotBlank
     private String name;
     private String description;
+    @NotNull
     private Double price;
     private Integer quantity;
 }

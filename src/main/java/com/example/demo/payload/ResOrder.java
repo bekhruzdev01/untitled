e12f0b7ef9aecf1f34361374bf339946eb318fe3
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-19 22:56:46 +0500

    Refactor ResOrder and remove ResOrderItem for improved payload structure

--- src/main/java/com/example/demo/payload/ResOrder.java
+++ src/main/java/com/example/demo/payload/ResOrder.java
@@ -1,5 +1,18 @@
 package com.example.demo.payload;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class ResOrder {
-    
+    private Long id;
+    private Long userId;
+    private String username;
+    private List<ResOrderItem> items;
+    private Double totalPrice;
+    private String status;
+    private String createdAt;
 }

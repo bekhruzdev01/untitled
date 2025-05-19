65bfa3df75bc5194735ff3c348d08399c6d7546f
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-19 22:57:27 +0500

    Refactor ResOrderItem: improve code readability and structure

--- src/main/java/com/example/demo/payload/ResOrderItem.java
+++ src/main/java/com/example/demo/payload/ResOrderItem.java
@@ -1,5 +1,14 @@
 package com.example.demo.payload;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResOrderItem {
-    
+    private Long id;
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private Double price;
 }

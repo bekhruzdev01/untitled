03f3b8b3f914dcccfbafdc4c1ec3e1f70d8e1958
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-20 11:45:02 +0500

    Update CountryDto: refine fields and add necessary getters/setters

--- demo/src/main/java/it/revo/demo/payload/CountryDto.java
+++ demo/src/main/java/it/revo/demo/payload/CountryDto.java
@@ -1,5 +1,41 @@
 package it.revo.demo.payload;
 
-public class CountryDto {
-    
+public class CountryDTO {
+    private Long id;
+    private String name;
+    private String capital;
+
+    public CountryDTO() {}
+
+    public CountryDTO(Long id, String name, String capital) {
+        this.id = id;
+        this.name = name;
+        this.capital = capital;
+    }
+
+    // Getter va Setter metodlari
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
 }

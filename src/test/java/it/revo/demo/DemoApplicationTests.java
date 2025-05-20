f7dcaf7f7929f32a2dc8456b4d51f6330bf66fd5
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-20 23:51:30 +0500

    Create Backend New Project

---
+++ src/test/java/it/revo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.revo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

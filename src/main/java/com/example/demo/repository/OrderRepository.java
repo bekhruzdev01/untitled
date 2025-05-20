135494783cb15247a62b1ee1b241ddd60501eb91
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-20 23:40:40 +0500

    Create Frontend with Vite + React
    Vite + React frontend created from scratch.

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    // Qo‘shimcha metodlar kerak bo‘lsa shu yerda yozing
-}
--- src/main/java/com/example/demo/repository/WishlistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Wishlist;
-
-public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
-    
-}

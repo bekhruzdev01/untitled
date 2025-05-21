0cf6f61f3919007a88e580d7a2a284ee8dd18f61
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-21 23:01:08 +0500

    Update CountryController with necessary improvements

--- demo/src/main/java/it/revo/demo/controler/CountryController.java
+++ demo/src/main/java/it/revo/demo/controler/CountryController.java
@@ -1,15 +1,22 @@
 package it.revo.demo.controler;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.revo.demo.entity.Countrty;
+import it.revo.demo.repository.CountryRepository;
+
 @RestController
 @RequestMapping("/country")
 public class CountryController {
-    
+    @Autowired
+    CountryRepository countryRepository;
     @GetMapping("/list")
     public List<Countrty> getAllCountries() {
-        return CountryRepository.findAll();
+        return countryRepository.findAll();
     }
 }

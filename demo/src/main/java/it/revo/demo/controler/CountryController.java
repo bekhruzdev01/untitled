4380821ee87d4275795bc5eda06c19e3e5779d5e
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-21 18:56:41 +0500

    Add CountryRepository and update CountryController for database operations

--- demo/src/main/java/it/revo/demo/controler/CountryController.java
+++ demo/src/main/java/it/revo/demo/controler/CountryController.java
@@ -1,8 +1,10 @@
 package it.revo.demo.controler;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/country")
 public class CountryController {
     
 }
---
+++ demo/src/main/java/it/revo/demo/repository/CountryRepository.java
@@ -0,0 +1,8 @@
+package it.revo.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository{
+
+    
+}

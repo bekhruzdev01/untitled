6d0d4a8a5e7c25615bae8bcf9ae074efb67b8648
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-21 23:55:40 +0500

    Refactor CountryDto: update fields and accessors

--- demo/src/main/java/it/revo/demo/payload/CountryDto.java
+++ demo/src/main/java/it/revo/demo/payload/CountryDto.java
@@ -1,41 +1,14 @@
-package it.revo.demo.payload;
+package it.revo.demo.controler;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountryDTO {
     private Long id;
     private String name;
-    private String capital;
-
-    public CountryDTO() {}
-
-    public CountryDTO(Long id, String name, String capital) {
-        this.id = id;
-        this.name = name;
-        this.capital = capital;
-    }
-
-    // Getter va Setter metodlari
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCapital() {
-        return capital;
-    }
-
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
+    private String code;
 }

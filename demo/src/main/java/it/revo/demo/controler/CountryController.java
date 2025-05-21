4f3b8194e9ab501a2d033cfab78b910528e4281c
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-21 18:49:53 +0500

    Fix: Correct typos and update logic in CountryController and Countrty entity

--- demo/src/main/java/it/revo/demo/controler/CountryController.java
+++ demo/src/main/java/it/revo/demo/controler/CountryController.java
@@ -1,5 +1,8 @@
 package it.revo.demo.controler;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CountryController {
     
 }
--- demo/src/main/java/it/revo/demo/entity/Countrty.java
+++ demo/src/main/java/it/revo/demo/entity/Countrty.java
@@ -1,3 +1,27 @@
+package it.revo.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+@Getter
+@Setter
+@Builder
 public class Countrty {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Double peopleSize;
 }

b82323b1e7156f24f35c8b1a7efdb1b35c351424
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-06-04 23:59:37 +0500

    Update ProfileRole entity implementation

--- demo/src/main/java/it/revo/demo/entity/ProfileRole.java
+++ demo/src/main/java/it/revo/demo/entity/ProfileRole.java
@@ -2,16 +2,22 @@ package it.revo.demo.entity;
 
 import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 public class ProfileRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     private Profile profile;
+
     @Column(nullable = false)
-    private Long UserId;
+    private Long profileId;
+
+    private Boolean 
 }
